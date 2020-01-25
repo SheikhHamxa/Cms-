97df8cbc7ff6ad0017c7fba52cc729da92384e32
SheikhHamxa <60283257+SheikhHamxa@users.noreply.github.com>
2020-01-25 13:18:09 +0500

    User of cms
    User of cms

--- models.py
+++ models.py
@@ -1,25 +1,60 @@
 from django.db import models
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
 
-# from USer.models import USer
-from location.models import Location, Franchise
-# from USer.models import USer
 
-# Create your models here.
-# from location.models import  Location
+# from package.models import Package, PackageRates
 # from vehicle.models import Vehicle
+# from vehicle.models import Vehicle
+from location.models import Franchise
+# from vehicle.models import Vehicle
+from vehicle.models import Vehicle
+
+LEXERS = [item for item in get_all_lexers() if item[1]]
+STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 
 
-class PackageRates(models.Model):
-    price_package_title = (
-        ('60 rupees', 'BOOK/COPY_Size for less than 500 gram of book/copy'),
-        ('180 rupees of book/copy', 'BOOK/COPY_Size between 500 gm To 1000 gram '),
-        ('250 rupees of book/copy', 'BOOK/COPY_Size between 1000 gm To 2000 gram'),
-        ('45 rupees of documents', 'DOCUMENTS_SIZE less than 30 gram '),
-        ('70 rupees of documents', 'DOCUMENTS_SIZE  between 30gm To 100 gram  '),
-        ('170 rupees of documents', 'DOCUMENTS_Size  between 100 To 200 gram '),
-        ('70 rupees of Other physical material', 'OTHER_PHYSICAL_INSTRUMENTS_Size less than 300 gram  '),
-        ('170 rupees of physical material', 'OTHER_PHYSICAL_INSTRUMENTS_Size between 300 to 1000 gram for other'),
-        ('200 rupees other physical material', 'OTHER_PHYSICLA_INSTRUMENTS_Size more than 1000 gram ')
+class UserType(models.Model):
+    User_Type = (
+        ('SENDER', 'Sender'),
+        ('RECEIVER', 'Receiver'),
+        ('MANAGER', 'Manager'),
+        ('STAFF', 'Staff'),
+        ('ADMIN', 'Admin'),
+        ('DRIVER', 'Driver'),
+        ('POST_PERSON', 'post_person'),
+
+    )
+    user_type = models.CharField(max_length=15, choices=User_Type, unique=True)
+    owner = models.ForeignKey('auth.User', related_name='usertype', on_delete=models.CASCADE)
+
+    def __str__(self):
+        template = '{0.user_type} '
+        return template.format(self)
+
+
+"""
+    sender = models.CharField(max_length=100)
+    receiver = models.CharField(max_length=100)
+    post_person = models.CharField(max_length=100)
+    staff = models.CharField(max_length=100)
+    manager = models.CharField(max_length=100)
+    Admin = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.sender
+"""
+
+
+class USer(models.Model):
+    select_gender = (('M', 'MALE'),
+                     ('F', 'FEMALE'),
+                     )
+    status = (
+        ('Punjab', 'PUNJAB'),
+        ('Sindh', 'SINDH'),
+        ('Balochistan', 'BALOCHISTAN'),
+        ('kpk', 'KPK'),
     )
     dist = (
         ('Rawalpindi', 'RAWALPINDI'),
@@ -78,92 +113,104 @@ class PackageRates(models.Model):
         ('Rawalakot', 'RAWALAKOT'),
 
     )
-    price_per_gram = models.CharField(max_length=100, choices=price_package_title)
-    from_district = models.CharField(max_length=100, blank=True, null=True, choices=dist)
-    to_district = models.CharField(max_length=100, blank=True, null=True,choices=dist)
-    # location = models.ManyToManyField(Location, related_name='packagerates')
-    owner = models.ForeignKey('auth.User', related_name='packagerates', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(unique=True, null=True)
+    city = models.CharField(max_length=100, blank=True)
+    phone_number = models.CharField(max_length=100, null=True)
+    cnic = models.CharField(max_length=100, unique=True, null=True)
+    gender = models.CharField(max_length=1, choices=select_gender)
+    province = models.CharField(max_length=100, choices=status)
+    district = models.CharField(max_length=100, choices=dist)
+    sector = models.CharField(max_length=100, blank=True)
+    street = models.CharField(max_length=100, blank=True)
+    house_no = models.CharField(max_length=100, blank=True)
+    owner = models.ForeignKey('auth.User', related_name='user', on_delete=models.CASCADE)
+    driver_liesence_number=models.CharField(max_length=100, null=True)
+    sender = models.ForeignKey('self', on_delete=models.CASCADE, related_name='a_user', null=True, blank=True)
+    receiver = models.ForeignKey('self', on_delete=models.CASCADE, related_name='bb_user', null=True)
+    manager = models.ForeignKey('self', on_delete=models.CASCADE, related_name='cc_user',blank=True, null=True)
+    admin = models.ForeignKey('self', on_delete=models.CASCADE, related_name='aaaaa_user', null=True, blank=True)
+    driver = models.ForeignKey('self', on_delete=models.CASCADE, related_name='e_user',blank=True,null=True)
+    post_person = models.ForeignKey('self', on_delete=models.CASCADE, related_name='f_user',null=True)
+    staff = models.ForeignKey('self', on_delete=models.CASCADE, related_name='g_user', null=True)
+    usertype = models.ForeignKey(UserType, related_name='user', on_delete=models.CASCADE,null=True)
+    # "" package=models.ForeignKey(Package,related_name='package',on_delete=models.CASCADE)
+    franchise = models.ForeignKey(Franchise, related_name='user', on_delete=models.CASCADE,null=True)
+    vehicle = models.ManyToManyField(Vehicle,related_name='vehicle', blank=True)
+    # location = models.ForeignKey(Location, related_name='user', on_delete=models.CASCADE)
+    # packagerates = models.ForeignKey(PackageRates, related_name='user', on_delete=models.CASCADE)
+    # highlighted = models.TextField(default=None)
+    # class Meta:
+    # ordering = ['created', ]
 
     def __str__(self):
-        template = '{0.from_district}  {0.to_district}'
+        template = '{0.first_name} {0.last_name} {0.email} '
         return template.format(self)
 
 
-class PackageStatus(models.Model):
-    package_status = (
-        ('PENDING', 'Pending'),
-        ('READY_FOR_PICKUP', 'Ready_For_Pickup'),
-        ('ON_THE_WAY', 'On_Way'),
-        ('AT_FRANCHISE', 'At_Franchise'),
-        ('READY_FOR_SHIPMENT', 'Ready_For_Shipment'),
-        ('SHIPMENT_GOES', 'Shipment_Goes')
-    )
-    status = models.CharField(max_length=100, choices=package_status, unique=True)
-    owner = models.ForeignKey('auth.User', related_name='packagestatus', on_delete=models.CASCADE)
+"""
+    def save(self, *args, **kwargs):  # new
+        
+        
 
-    # "" package = models.OneToOneField(Package, on_delete=models.CASCADE, primary_key=True)
+        lexer = get_lexer_by_name(self.language)
+        linenos = 'table' if self.linenos else False
+        options = {'title': self.title} if self.title else {}
+        formatter = HtmlFormatter(style=self.style, linenos=linenos,
+                                  full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super(Employe, self).save(*args, **kwargs)
+"""
+
+"""
+class Sender(models.Model):
+    province = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    sector = models.CharField(max_length=100)
+    street = models.CharField(max_length=100)
+    house_no = models.CharField(max_length=100)
+    USer = models.ForeignKey(Employe, related_name='sender', on_delete=models.CASCADE)
+    owner = models.ForeignKey('auth.User', related_name='sender', on_delete=models.CASCADE)
 
     def __str__(self):
-        template = ' {0.status}'
-        return template.format(self)
+        return self.USer.first_name
 
 
-class Package(models.Model):
-    package_title = (
-        ("BOOK/COpy", 'Book/Copy'),
-        ('DOCUMENTS', 'Documents'),
-        ('OTHER_PHYSICAL_INSTRUMENTS', "Other_Physical_Instruments")
-
-    )
-
-    name = models.CharField(max_length=100, choices=package_title)
-    # item_type = models.TextField()
-    arrivel_date = models.DateField()
-    owner = models.ForeignKey('auth.User', related_name='package', on_delete=models.CASCADE)
-    packagerates = models.ForeignKey(PackageRates, related_name='package', on_delete=models.CASCADE)
-    # ""vehicle = models.ForeignKey(Vehicle, related_name='package', on_delete=models.CASCADE)
-    # user = models.ForeignKey('USer.USer', related_name='package', on_delete=models.CASCADE)
-    # USer=models.ForeignKey(Employe, related_name='package', on_delete=models.CASCADE)
-    packagestatus = models.ForeignKey(PackageStatus, related_name='package', on_delete=models.CASCADE)
-    sender = models.ForeignKey('USer.USer', related_name='package', on_delete=models.CASCADE,null=True)
-    receiver = models.ForeignKey('USer.USer', related_name='b_package', on_delete=models.CASCADE,null=True)
-    post_person = models.ForeignKey('USer.USer', related_name='c_package', on_delete=models.CASCADE, null=True)
+class Receiver(models.Model):
+    province = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    sector = models.CharField(max_length=100)
+    street = models.CharField(max_length=100)
+    house_no = models.CharField(max_length=100)
+    USer = models.ForeignKey(Employe, related_name='receiver', on_delete=models.CASCADE)
+    owner = models.ForeignKey('auth.User', related_name='receiver', on_delete=models.CASCADE)
+    sender = models.ManyToManyField(Sender)
 
     def __str__(self):
-        template = '{0.name} {0.packagerates}'
-        return template.format(self)
-
-
-class PackageBilling(models.Model):
-    owner = models.ForeignKey('auth.User', related_name='packagebilling', on_delete=models.CASCADE)
-
-    # bill_pay=models.CharField(max_length=100, unique=True)
-    package = models.OneToOneField(Package, on_delete=models.CASCADE, primary_key=True)
-    franchise = models.ForeignKey(Franchise, related_name='packagebilling', on_delete=models.CASCADE)
-    """
-    def __str__(self):
-        return self.package.name
-"""
-
+        return self.USer.first_name
 
 """
-
-
-Status_Package = (
-    ('pending', 'PENDING'),
-    ('ready_for_shipment', 'READY_FOR_SHIPMENT'),
-    ('on_the_way', 'ON_THE_WAY'),
-    ('at_franchise', 'AT_FRANCHISE'),
-    ('ready_for_pickup', 'READY_FOR_PICKUP'),
-)
 """
+class PostPerson(models.Model):
+    USer = models.ForeignKey(Employe, related_name='postperson', on_delete=models.CASCADE)
+    owner = models.ForeignKey('auth.User', related_name='postperson', on_delete=models.CASCADE)
+    employe_number = models.IntegerField()
+    duty_tyming = models.DateTimeField()
 
-"""
-class PackageStatus(models.Model):
-    status = models.CharField(max_length=15)
-    package = models.ForeignKey(Package, related_name='package_status', on_delete=models.CASCADE)
-    owner = models.ForeignKey('auth.User', related_name='package_status', on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.status
-"""
+class Manager(models.Model):
+    USer = models.ForeignKey(Employe, related_name='manager', on_delete=models.CASCADE)
+    owner = models.ForeignKey('auth.User', related_name='manager', on_delete=models.CASCADE)
+    employe_number = models.IntegerField()
+    duty_tyming = models.DateTimeField()
+
+
+class Staff(models.Model):
+    USer = models.ForeignKey(Employe, related_name='staff', on_delete=models.CASCADE)
+    owner = models.ForeignKey('auth.User', related_name='staff', on_delete=models.CASCADE)
+    employe_number = models.IntegerField()
+    duty_tyming = models.DateTimeField()
+    
+"""""
--- serializations.py
+++ serializations.py
@@ -1,66 +1,117 @@
 from rest_framework import serializers
-from django.db import models
-
 from location.models import Franchise
-from location.serializations import FranchiseSerializer
 from vehicle.models import Vehicle
-from .models import Package, PackageRates, PackageStatus, PackageBilling
+from .models import USer
+# from .models import Sender
+from .models import UserType
+# from .models import Receiver
+# from .models import Manager
+# from .models import PostPerson
+# from .models import Staff
 
 from django.contrib.auth.models import User
-from USer.models import USer
 
 
-class PackageSerializer(serializers.ModelSerializer):
-    owner = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field='username')
-    packagerates = serializers.SlugRelatedField(queryset=PackageRates.objects.all(), slug_field='price_per_gram')
-    # vehicle = serializers.SlugRelatedField(queryset=Vehicle.objects.all(), slug_field='name')
-    sender = serializers.SlugRelatedField(queryset=USer.objects.filter(usertype=1), slug_field='first_name', allow_null=True)
-    receiver = serializers.SlugRelatedField(queryset=USer.objects.filter(usertype=2), slug_field='last_name',allow_null=True)
-    post_person = serializers.SlugRelatedField(queryset=USer.objects.filter(usertype=6), slug_field='email',allow_null=True)
+class USerSerializer(serializers.ModelSerializer):
+    sender=serializers.SlugRelatedField(queryset=USer.objects.filter(usertype=1), slug_field='first_name', allow_null=True)
+    receiver=serializers.SlugRelatedField(queryset=USer.objects.filter(usertype=2), slug_field='first_name', allow_null=True)
+    manager=serializers.SlugRelatedField(queryset=USer.objects.filter(usertype=3), slug_field='first_name', allow_null=True)
+    staff=serializers.SlugRelatedField(queryset=USer.objects.filter(usertype=4), slug_field='first_name', allow_null=True)
+    admin=serializers.SlugRelatedField(queryset=USer.objects.filter(usertype=5), slug_field='first_name', allow_null=True)
+    driver=serializers.SlugRelatedField(queryset=USer.objects.filter(usertype=6), slug_field='first_name', allow_null=True)
+    post_person=serializers.SlugRelatedField(queryset=USer.objects.filter(usertype=7), slug_field='first_name', allow_null=True)
+    owner = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field='username', allow_empty=True,
+                                         allow_null=True)
+    usertype = serializers.SlugRelatedField(queryset=UserType.objects.all(), slug_field='user_type')
+    franchise = serializers.SlugRelatedField(queryset=Franchise.objects.all(), slug_field='name')
 
-    packagestatus = serializers.SlugRelatedField(queryset=PackageStatus.objects.all(), slug_field='status')
+    # vehicle = serializers.SlugRelatedField(queryset=Vehicle.objects.all(),slug_field='name',allow_empty=True, allow_null=True)
+    # vehicle=VehicleSerializer(Vehicle, allow_null=True, read_only=True)
+    # vehicle = serializers.SlugRelatedField(queryset=Vehicle.objects.all(),slug_field='name',allow_empty=True, allow_null=True)
 
-    # usertype = serializers.StringRelatedField(many=True, read_only=True)
-    # owner = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
-    # packagestatus = serializers.StringRelatedField(many=True,read_only=True)
-    # user = serializers.StringRelatedField(read_only=True)
-    class Meta:
-        model = Package
-        fields = '__all__'
-
-
-class PackageBillingSerializer(serializers.ModelSerializer):
-    owner = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field='username')
-    franchise = serializers.SlugRelatedField(queryset=Franchise.objects.all(), slug_field='email')
-    # franchise=FranchiseSerializer(read_only=True)
-    # package=PackageSerializer(read_only=True)
-    package = serializers.SlugRelatedField(queryset=Package.objects.all(), slug_field='name')
-
+    # franchise= FranchiseSerializer(read_only=True)
+    # "" packagerates = serializers.SlugRelatedField(queryset=PackageRates.objects.all(), slug_field='price_per_gram')
+    # vehicle = serializers.SlugRelatedField(queryset=Vehicle.objects.all(), slug_field='name', many=True)
+    # vehicle = VehicleSerializer(allow_null=True)
+    # postperson = serializers.StringRelatedField(many=True, read_only=True)
+    # manager = serializers.StringRelatedField(many=True, read_only=True)
+    # staff = serializers.StringRelatedField(many=True, read_only=True)
+    # package = serializers.StringRelatedField(many=True, read_only=True)
     # owner = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
 
     class Meta:
-        model = PackageBilling
+        model = USer
         fields = '__all__'
 
 
-class PackageRatesSerializer(serializers.ModelSerializer):
+class UserTypeSerializer(serializers.ModelSerializer):
     owner = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field='username')
+
     # owner = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
     # user = serializers.StringRelatedField(many=True, read_only=True)
-    # package = serializers.StringRelatedField(many=True, read_only=True)
-    # location = serializers.StringRelatedField(many=True, read_only=True)
 
     class Meta:
-        model = PackageRates
+        model = UserType
         fields = '__all__'
 
 
-class PackageStatusSerializer(serializers.ModelSerializer):
-    owner = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field='username')
-    # owner = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
-    # franchise = models.OneToOneField(PackageRates, related_name='packagerates', on_delete=models.CASCADE)
-    # pacakage = serializers.SlugRelatedField(queryset=Package.objects.all(), slug_field='name')
+"""
+class SenderSerializer(serializers.ModelSerializer):
+    owner = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    receievr = serializers.StringRelatedField()
 
     class Meta:
-        model = PackageStatus
+        model = Sender
         fields = '__all__'
+
+
+class ReceiverSerializer(serializers.ModelSerializer):
+    owner = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+
+    class Meta:
+        model = Receiver
+        fields = '__all__'
+
+"""
+"""
+class PostPersonSerializer(serializers.ModelSerializer):
+    owner = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+
+    class Meta:
+        model = PostPerson
+        fields = '__all__'
+
+
+class ManagerSerializer(serializers.ModelSerializer):
+    owner = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+
+    class Meta:
+        model = Manager
+        fields = '__all__'
+
+
+class StaffSerializer(serializers.ModelSerializer):
+    owner = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+
+    class Meta:
+        model = Staff
+        fields = '__all__'
+"""
+"""sender = serializers.SlugRelatedField(queryset=USer.objects.filter(usertype=1), slug_field='first_name',
+                                      allow_empty=True, allow_null=True)
+
+receiver = serializers.SlugRelatedField(queryset=USer.objects.filter(usertype=2), slug_field='first_name',
+                                        allow_empty=True, allow_null=True)
+
+manager = serializers.SlugRelatedField(queryset=USer.objects.filter(usertype=3), slug_field='first_name',
+                                       allow_null=True)
+admin = serializers.SlugRelatedField(queryset=USer.objects.filter(usertype=4), slug_field='first_name',
+                                     allow_null=True, allow_empty=True)
+postperson = serializers.SlugRelatedField(queryset=USer.objects.filter(usertype=5), slug_field='first_name',
+                                          allow_null=, allow_empty=True)
+driver = serializers.SlugRelatedField(queryset=USer.objects.filter(usertype=6), slug_field='first_name',
+                                      allow_null=True)
+
+staff = serializers.SlugRelatedField(queryset=USer.objects.filter(usertype=7), slug_field='first_name',
+                                     allow_empty=True, allow_null=True)
+"""
